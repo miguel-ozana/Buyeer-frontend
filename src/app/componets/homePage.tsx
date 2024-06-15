f28f6b143b4c354e523ee26d29d705b69204fd1e
miguel-ozana <miguelozana74@gmail.com>
2024-06-14 23:34:12 -0300

    new commit for frontend

--- src/app/componets/homePage.tsx
+++ src/app/componets/homePage.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState, useEffect, useCallback } from "react";
 import { Trash2, Edit, CheckSquare, Square } from "react-feather";
 import axios from 'axios';
