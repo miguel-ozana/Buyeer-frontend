a4c54aad26296966196f0c601962e56e64e43aa7
miguel-ozana <miguelozana74@gmail.com>
2024-06-15 14:23:33 -0300

    frontend commit. DeviceId update

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,5 @@
 export function getDeviceId() {
-  console.log('Checking localStorage access');
   if (typeof window !== 'undefined') {
-    console.log('Accessing localStorage on client side');
     let deviceId = localStorage.getItem('deviceId');
     if (!deviceId) {
       deviceId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
@@ -9,6 +7,5 @@ export function getDeviceId() {
     }
     return deviceId;
   }
-  console.log('localStorage not available on server side');
   return null;
 }
